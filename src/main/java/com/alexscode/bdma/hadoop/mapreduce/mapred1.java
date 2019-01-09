282c7dd25fbd07d9b58f13578cdda6a31c624c14
Xairon <ringuetnicolas2@gmail.com>
2019-01-09 17:46:23 +0100

    add map config

--- src/main/java/com/alexscode/bdma/hadoop/mapreduce/mapred1.java
+++ src/main/java/com/alexscode/bdma/hadoop/mapreduce/mapred1.java
@@ -53,32 +53,5 @@ public class mapred1 {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        Configuration config = HBaseConfiguration.create();
-        Job job = Job.getInstance(config, "Q2");
-        job.setJarByClass(mapred1.class);
-        Scan scan = new Scan();
-        scan.setCaching(500);        // 1 is the default in Scan, which will be bad for MapReduce jobs
-        scan.setCacheBlocks(false);  // don't set to true for MR jobs
-// set other scan attrs
 
-        TableMapReduceUtil.initTableMapperJob(
-                "A",      // input table
-                scan,             // Scan instance to control CF and attribute selection
-                Mapper1.class,   // mapper class
-                ImmutableBytesWritable.class,             // mapper output key
-                IntWritable.class,             // mapper output value
-                job);
-        TableMapReduceUtil.initTableReducerJob(
-                "21402752Q2",      // output table
-                null,             // reducer class
-                //Reducer1.class,
-                job);
-        job.setNumReduceTasks(0);
-
-        boolean b = job.waitForCompletion(true);
-        if (!b) {
-            throw new IOException("error with job!");
-        }
-    }
 }
